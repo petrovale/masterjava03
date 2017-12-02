4ba104335f43ddff7311ef3e1db6dbb8b655966a
petrovale <leha.isakov@yandex.ru>
2017-12-03 02:09:09 +0300

    HW7 1 and 2 update mailService.wsdl

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -5,7 +5,7 @@ import com.google.common.collect.ImmutableSet;
 public class MailWSClientMain {
     public static void main(String[] args) {
         MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("To <masterjava@javaops.ru>")),
-                ImmutableSet.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("To <leha.isakov@yandex.ru>")),
+                ImmutableSet.of(new Addressee("Copy <leha.isakov@yandex.ru>")), "Subject", "Body");
     }
 }
