35cd7125695503c790cdce8a2d168131f4ed14af
petrovale <leha.isakov@yandex.ru>
2018-01-06 15:56:59 +0300

    HW10 jersey attach

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
@@ -1,15 +1,26 @@
 package ru.javaops.masterjava.service.mail.rest;
 
 
+import com.google.common.collect.ImmutableList;
+import org.glassfish.jersey.media.multipart.BodyPartEntity;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.hibernate.validator.constraints.NotBlank;
+import ru.javaops.masterjava.service.mail.Attachment;
 import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailServiceExecutor;
 import ru.javaops.masterjava.service.mail.MailWSClient;
+import ru.javaops.masterjava.service.mail.util.Attachments;
 import ru.javaops.masterjava.web.WebStateException;
 
+import javax.activation.DataHandler;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.Collections;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @Path("/")
 public class MailRS {
@@ -22,11 +33,27 @@ public class MailRS {
 
     @POST
     @Path("send")
+    @Consumes({MediaType.MULTIPART_FORM_DATA})
     @Produces(MediaType.APPLICATION_JSON)
-    public GroupResult send(@NotBlank @FormParam("users") String users,
-                            @FormParam("subject") String subject,
-                            @NotBlank @FormParam("body") String body) throws WebStateException {
+    public GroupResult send(@FormDataParam("attach") FormDataBodyPart attachBodyPart,
+                            @NotBlank @FormDataParam("users") String users,
+                            @FormDataParam("subject") String subject,
+                            @NotBlank @FormDataParam("body") String body) throws WebStateException, IOException {
+        final List<Attachment> attachments;
+        if (attachBodyPart == null) {
+            attachments = ImmutableList.of();
+        } else {
+            try {
+                String attachName = attachBodyPart.getContentDisposition().getFileName();
+//          UTF-8 encoding workaround: https://java.net/jira/browse/JERSEY-3032
+                String utf8name = new String(attachName.getBytes("ISO8859_1"), "UTF-8");
+                BodyPartEntity bodyPartEntity = ((BodyPartEntity) attachBodyPart.getEntity());
 
-        return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, Collections.emptyList());
+                attachments = ImmutableList.of(new Attachment(utf8name, new DataHandler((Attachments.ProxyDataSource) bodyPartEntity::getInputStream)));
+            } catch (UnsupportedEncodingException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, attachments);
     }
 }
